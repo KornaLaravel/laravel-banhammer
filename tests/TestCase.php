fc5f1e4b7791697b5c874dae2770b132a5fc61b3
mchev <martin.chevignard@gmail.com>
2024-05-24 09:31:20 +0200

    Updated workflow

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->publishAndRunMigrations();
-    }
-
     protected function getEnvironmentSetUp($app): void
     {
         // Load the .env file
@@ -28,18 +21,4 @@ abstract class TestCase extends Orchestra
             'Mchev\Banhammer\BanhammerServiceProvider',
         ];
     }
-
-    protected function publishAndRunMigrations(): void
-    {
-        // Publish the package migrations
-        $this->artisan('vendor:publish', [
-            '--provider' => 'Mchev\Banhammer\BanhammerServiceProvider',
-            '--tag' => 'banhammer-migrations',
-            '--force' => true,
-        ])->run();
-
-        // Run the migrations
-        $this->artisan('migrate', ['--database' => 'testing'])->run();
-    }
-
 }
